f3d218e0b578f004fd0e7b77c63c008573e1f1e0
Tapas1380 <tapasranjanhr@gmail.com>
2023-09-06 16:49:24 +0530

    Refernces

---
+++ References_App/p1/Address.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class Address {
+	public String city,state;
+	public int pinCode;
+	public void getAddress() {
+	System.out.println("====CustomerAddress=====");
+	System.out.println("City:"+city);
+	System.out.println("State:"+state);
+	System.out.println("PinCode:"+pinCode);
+	}
+
+}
---
+++ References_App/p2/DemoRef1.java
@@ -0,0 +1,28 @@
+package p2;
+import java.util.Scanner;
+import p1.*;
+public class DemoRef1 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		Customer c = new Customer();
+		System.out.println("Enter the CustId:");
+		c.cId = s.nextLine();
+		System.out.println("Enter the CustName:");
+		c.cName = s.nextLine();
+		System.out.println("Enter the CustCity:");
+		c.ad.city = s.nextLine();
+		System.out.println("Enter the CustState:");
+
+		c.ad.state = s.nextLine();
+		System.out.println("Enter the CustPinCode:");
+		c.ad.pinCode = s.nextInt();
+
+		c.getCustomer();
+		c.ad.getAddress();
+
+		s.close();
+
+	}
+
+}
