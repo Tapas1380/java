4a855e71dad3e0b42917f125dffaaa561b471771
Tapas1380 <tapasranjanhr@gmail.com>
2023-09-06 16:33:40 +0530

    Access modifier

---
+++ src/p2/B.java
@@ -0,0 +1,14 @@
+package p2;
+import p1.A;
+public class B {
+	public A ob = new A();//NonPrimitive
+	//Instance variable
+	public void dis()
+	{
+	System.out.println("Value a : "+ob.a);
+	//System.out.println("Value b : "+ob.b);
+	//System.out.println("Value c : "+ob.c);
+	//System.out.println("Value d : "+ob.d);
+	}
+
+}
